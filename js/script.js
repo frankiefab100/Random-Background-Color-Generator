6e531cf72c2ec1245fa15424255068d6309b6d88
Rishon D'Silva <rishonalton@gmail.com>
2021-10-01 23:36:42 +0530

    Bug Fixed

--- js/script.js
+++ js/script.js
@@ -2,37 +2,44 @@ const colorBtn = document.getElementById("colorBtn");
 const hexColor = document.querySelector(".hexColor");
 const rgbColor = document.querySelector(".rgbColor");
 
-colorBtn.addEventListener("click", () => {
-  // Deriving the Hexcode
-  function getRandomValues() {
-    let hexValues = "01234567890ABCDEF";
-    let hexCodes = "#";
+
+const getHexCode = () => {
+
+    const hexValues = "01234567890ABCDEF";
+    let hexCode = "#";
 
     for (let i = 0; i < 6; i++) {
-      hexCodes += hexValues[Math.floor(Math.random() * 16)];
-      hexColor.textContent = hexCodes;
-      document.body.style.backgroundColor = hexCodes;
+      hexCode += hexValues[Math.floor(Math.random() * 16)];
     }
-    return color;
-  }
-  getRandomValues();
 
-  let hexCode1 = hexValues[Math.trunc(color / 16)];
-  let hexCode2 = hexValues[color % 16];
-  function getHexValues(color) {
-    return String(hexCode1) + String(hexCode2);
-  }
+    return hexCode;
 
-  hexColor.innerHTML = `#${getHexValues(red)}${getHexValues(
-    green
-  )}${getHexValues(blue)}`;
+}
 
-  // Deriving the RGB code
-  let red = Math.floor(Math.random() * 256);
-  let green = Math.floor(Math.random() * 256);
-  let blue = Math.floor(Math.random() * 256);
-  let rgbValues = `${red}, ${green}, ${blue}`;
 
-  document.body.style.backgroundColor = rgbValues;
-  rgbColor.innerHTML = ` rgb(${rgbValues})`;
+const hexToRgb = (hex) => {
+
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+
+}
+
+
+colorBtn.addEventListener("click", () => {
+
+  const hexCode = getHexCode()
+
+  const red = hexToRgb(hexCode).r
+  const green = hexToRgb(hexCode).g
+  const blue = hexToRgb(hexCode).b
+
+  hexColor.textContent = hexCode
+  document.body.style.backgroundColor = hexCode
+  rgbColor.innerHTML = ` RGB(${red}, ${green}, ${blue})`;
+
 });
